33765b05bdda0550812c7a05c8a75d6ebfa7c4e6
phamthuynek <1954052097thuy@ou.edu.vn>
2021-11-02 14:29:56 +0700

    update form1

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,15 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
